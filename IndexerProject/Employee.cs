da8c43ffda36f6f0713d5b82857f655ea073d68b
sameer singh <v-samesingh@microsoft.com>
2024-07-01 18:20:05 +0530

    Add project files.

---
+++ IndexerProject/Employee.cs
@@ -0,0 +1,72 @@
+﻿using System;
+namespace IndexerProject
+{
+    class Employee
+    {
+        int EmpId;
+        string Ename;
+        string Location;
+        double Salary;
+        public string DName { get; set; }
+
+        public Employee(int EmpId, string Ename, string Location, double Salary, string dName)
+        {
+            this.EmpId = EmpId;
+            this.Ename = Ename;
+            this.Location = Location;
+            this.Salary = Salary;
+            DName = dName;
+        }
+        public object this[int index]
+        {
+            get
+            {
+                if (index == 0)
+                {
+                    return EmpId;
+                }
+                else if (index == 1)
+                {
+                    return Ename;
+                }
+                else if (index == 2)
+                {
+                    return Location;
+                }
+                else if (index == 3)
+                {
+                    return Salary;
+                }
+                else if (index == 4)
+                {
+                    return DName;
+                }
+                return null;
+            }
+            set
+            {
+                if (index == 0)
+                {
+                    EmpId = (int)value;
+                }
+                else if (index == 1)
+                {
+                    Ename = (string)value;
+                }
+                else if (index == 2)
+                {
+                    Location = (string)value;
+                }
+                else if (index == 3)
+                {
+                    Salary = (double)value;
+                }
+                else if (index == 4)
+                {
+                    DName = (string)value;
+                }
+            }
+        }
+    }
+}
+
---
+++ IndexerProject/TestEmployee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace IndexerProject
+{
+     class TestEmployee
+    {
+        static void Main()
+        {
+            Employee emp=new Employee(102,"Sameer","Bangalore",50000,"IT");
+            Console.WriteLine(emp[0]);   //once we define Indexer in a class, the class behaves like an virtual array
+            Console.WriteLine(emp[1]);
+            Console.WriteLine(emp[2]);
+            Console.WriteLine(emp[3]);
+            Console.WriteLine(emp[4]);
+
+            emp[1] = "Sameer Singh";
+            Console.WriteLine(emp[1]);
+
+        }
+    }
+}
